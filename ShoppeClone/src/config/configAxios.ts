8d89d30078c68169d3de6d8dee18af96ca107323
Kane.Do <Kane.Do@avepoint.com>
2024-03-18 10:20:29 +0700

    [S-0017] Catch status code dispaly toast message

--- ShoppeClone/src/config/configAxios.ts
+++ ShoppeClone/src/config/configAxios.ts
@@ -1,15 +1,29 @@
-import axios, { AxiosInstance } from "axios";
+import axios, { AxiosError, AxiosInstance } from "axios";
+import { toast } from "react-toastify";
+import { HttpStatusCode } from "src/utils/constants";
 
 class fetHandlerAxios {
   instance: AxiosInstance;
   constructor() {
     this.instance = axios.create({
       baseURL: "https://api-ecom.duthanhduoc.com/",
-      timeout: 3000,
+      timeout: 5000,
       headers: {
         "Content-Type": "application/json"
       }
     });
+    this.instance.interceptors.response.use(
+      function (response) {
+        return response;
+      },
+      function (error: AxiosError) {
+        if (error.response?.status !== HttpStatusCode.UnprocessableEntity) {
+          const data: any | undefined = error.response?.data;
+          const message = data.message || error.message;
+          toast.error(message);
+        }
+      }
+    );
   }
 }
 
