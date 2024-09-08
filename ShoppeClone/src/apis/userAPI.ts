f1abc095ef93016f797d06723190311f2f058ca7
Kane.Do <Kane.Do@avepoint.com>
2024-09-08 15:58:45 +0700

    [S-0055] API User

---
+++ ShoppeClone/src/apis/userAPI.ts
@@ -0,0 +1,22 @@
+import { User } from "src/auth/models";
+import fetHandler from "src/config/configAxios";
+import { SuccessResponse } from "src/types/utils.type";
+
+interface BodyUpdateProfile extends Omit<User, "_id" | "roles" | "createdAt" | "updatedAt" | "email"> {
+  password?: string;
+  newPassword?: string;
+}
+export const getProfile = () => {
+  return fetHandler.get<SuccessResponse<User>>("me");
+};
+export const updateProfile = (body: BodyUpdateProfile) => {
+  return fetHandler.post<SuccessResponse<User>>("user", body);
+};
+
+export const uploadAvatar = (body: FormData) => {
+  return fetHandler.post<SuccessResponse<string>>("user/upload-avatar", body, {
+    headers: {
+      "Content-Type": "multipart/form-data"
+    }
+  });
+};
--- ShoppeClone/src/auth/models.ts
+++ ShoppeClone/src/auth/models.ts
@@ -3,8 +3,9 @@ export interface User {
   _id: string;
   roles: Role[];
   email: string;
-  name: string;
-  date_of_birth: null;
+  name?: string;
+  date_of_birth?: string;
+  avatar?: string;
   address: string;
   phone: string;
   createdAt: string;
